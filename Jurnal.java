ae749dad3abb57f3d0b9dc22a9c2316587212855
Gilar6706160027 <37774151+Gilar6706160027@users.noreply.github.com>
2018-03-26 09:14:41 +0700

    Add files via upload

---
+++ Jurnal.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jurnal;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Praktikan
+ */
+public class Jurnal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner show = new Scanner(System.in);
+        System.out.print("Masukan Jumlah Fibonacci : ");
+        int n = show.nextInt();
+        int a = 0 ;
+        int b = 1 ;
+        int jumlah ;
+         
+        for(int i = 1; i <=n; i++) {
+            System.out.print(b + " ");
+            b=b+a ;
+            a=b-a ;
+        }
+        jumlah = b++ ;
+        System.out.println(jumlah);
+    }
+    
+}
